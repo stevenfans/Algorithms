2a07d6584df9b180877f2fc132081d78645fe4eb
Steven Phan <steven.phan009@gmail.com>
2020-01-27 12:30:18 -0800

    initialized new files

---
+++ InsertionSort.java
@@ -0,0 +1,8 @@
+// Write an insertion sort algorithm wth: 
+//     1.  using doubly linked list
+//     2.  able to sort lower case string words alphabetically
+//     3.  able to sort up to 20 words
+
+public class InsertionSort{
+    
+}
