a73745bca13f0ae513b9f45e2823b26c6d5d163e
Steven Phan <steven.phan009@gmail.com>
2020-01-28 10:39:31 -0800

    added remove and insertbefore functions for doubly linked list

--- InsertionSort/DoublyLinkedList.java
+++ InsertionSort/DoublyLinkedList.java
@@ -27,10 +27,8 @@ public class DoublyLinkedList{
     }
     
     public void addinFront(String n) {
-        
         //create a new node
         Node temp = new Node(n);
-        
         //next point of the new node points to the head of the list
         temp.next = head; 
         //make the previous point to null
@@ -42,6 +40,25 @@ public class DoublyLinkedList{
         }
 
         head = temp; 
+    }
+
+    //find the node that matches the data and remove it
+    public void removeNode(String nodeData){
+        
+        //create node starting head
+        Node startingHead = head; 
+
+        //traverse linked list and find the node
+        while(startingHead.data != nodeData){
+            startingHead = startingHead.next; 
+        }
+        // create a new node pointer to before the remove node location 
+        Node beforeRemoveNode = startingHead; 
+        beforeRemoveNode = beforeRemoveNode.prev;
+
+        beforeRemoveNode.next = startingHead.next; 
+        startingHead= startingHead.next; 
+        startingHead.prev = beforeRemoveNode; 
     }
 
     public static DoublyLinkedList insertNode(DoublyLinkedList list, int locationToReplace, String newData) {
@@ -59,11 +76,6 @@ public class DoublyLinkedList{
             i++; 
         }
 
-        //now at the element that you want to replace
-       // System.out.println(startingHead.data);
-        
-        //startingHead = startingHead.prev;
-        //System.out.println("Starting Head:"+startingHead.data);
         Node trail = startingHead; //trail is old location 
         
         //traverse one more back
@@ -72,16 +84,40 @@ public class DoublyLinkedList{
         tmp.prev = startingHead; 
         tmp.next = trail; 
         trail.prev = tmp.next;
-        
-
-        //make the new node equal to the previous pointer of startingHead
-        
-       // System.out.println(tmp.data);
 
         return tmpList;
     }
 
-    public static void printList(Node n){
+    // function creates a Node for the new data and places behind a specified data 
+    // in the linked list
+    public void insertNodeBefore(String insertBeforeThisData , String newData){
+
+        //create a temporary list 
+        //DoublyLinkedList tmpList = list; 
+        Node startingHead = head;
+
+        //get the starting head to the location of the node to be moved
+        while(startingHead.data != insertBeforeThisData){
+            startingHead = startingHead.next; 
+        }
+
+        //create a Nod for the new data
+        Node newNode = new Node(newData);
+
+        //fix the pointers
+        newNode.prev = startingHead.prev; 
+        newNode.next = startingHead;
+        startingHead.prev = newNode; 
+        startingHead = startingHead.prev;
+        newNode = newNode.prev;
+       // startingHead = startingHead.prev; 
+        newNode.next = startingHead; 
+
+    }
+
+    public void printList(){
+
+        Node n = head;
         // check if the current node not null, print data
         while(n.data!=null){
             System.out.println(n.data); 
@@ -90,22 +126,21 @@ public class DoublyLinkedList{
         }
     }
 
-    //to test class
-    public static void main(String[]args){
+    // //to test class
+    // public static void main(String[]args){
         
-        //System.out.println("tst");
+    //     //System.out.println("tst");
 
-        // create an empty linked list
-        DoublyLinkedList dll = new DoublyLinkedList(); 
-        dll.addinFront("apple");
-        dll.addinFront("test");
-        dll.addinFront("going");
-        dll.addinFront("zeebra");
-        dll.addinFront("aardvark");
-        //System.out.printnln("asdf");
-        //printList(dll.head); 
-        insertNode(dll,3, "newData");
-        printList(dll.head);
-        
-    }
+    //     // create an empty linked list
+    //     DoublyLinkedList dll = new DoublyLinkedList(); 
+    //     dll.addinFront("apple");
+    //     dll.addinFront("test");
+    //     dll.addinFront("going");
+    //     dll.addinFront("zeebra");
+    //     dll.addinFront("aardvark");
+    //     //System.out.printnln("asdf");
+    //     //printList(dll.head); 
+    //     insertNode(dll,3, "newData");
+    //     printList(dll.head);
+    // }
 }
--- InsertionSort/InsertionSort.java
+++ InsertionSort/InsertionSort.java
@@ -3,11 +3,22 @@
 // //     2.  able to sort lower case string words alphabetically
 // //     3.  able to sort up to 20 words
 
-// public class InsertionSort{
+public class InsertionSort{
     
-//     public static void main(String[]args){
+    public static void main(String[]args){
 
-//         String test = "test"; 
-//         DoublyLinkedList dll = new DoublyLinkedList();
-//     }
-// }
+        // create an empty linked list
+        DoublyLinkedList dll = new DoublyLinkedList(); 
+        dll.addinFront("apple");
+        dll.addinFront("test");
+        dll.addinFront("going");
+        dll.addinFront("zeebra");
+        dll.addinFront("aardvark");
+        dll.removeNode("going");
+        dll.insertNodeBefore("test", "fuck");
+        dll.printList(); 
+        //dll.insertNode(dll,3, "newData");
+        //dll.printList(dll.head);
+        //dll.insertNodeBefore(1, 3);
+    }
+}
