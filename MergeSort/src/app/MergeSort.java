b00a5a36b28141a1fe907bfda799c65d5baabbf5
Steven Phan <steven.phan009@gmail.com>
2020-02-21 23:03:58 -0800

    added recursion to mergesort function

--- MergeSort/src/app/MergeSort.java
+++ MergeSort/src/app/MergeSort.java
@@ -2,14 +2,20 @@ package app;
 
 public class MergeSort{
 
-    public static void mergesort(String[] A, left, right){
+    public static void mergeSort(Integer[] A, left, right){
 
+        if (left<right){
+            middle = (left+right)/2;
+            mergeSort(A,left,middle);
+            mergeSort(A,middle+1,right);
+            merge(A,left,middle,right);
+        }
     }
 
-    public static void merge(String[] A, left, middle, right){
-
+    public static void merge(Integer[] A, left, middle, right){
+        
     }
-    public static void main(String[] args){
+    public static void main(Integer[] args){
         System.out.println("Hello World");
     }
 }
