6068ded6d19ae71482226ba01293cd511a77eb74
Steven Phan <steven.phan009@gmail.com>
2020-02-21 22:47:50 -0800

    added merge functions

--- MergeSort/src/app/MergeSort.java
+++ MergeSort/src/app/MergeSort.java
@@ -2,7 +2,13 @@ package app;
 
 public class MergeSort{
 
-    public static void merge()
+    public static void mergesort(String[] A, left, right){
+
+    }
+
+    public static void merge(String[] A, left, middle, right){
+
+    }
     public static void main(String[] args){
         System.out.println("Hello World");
     }
