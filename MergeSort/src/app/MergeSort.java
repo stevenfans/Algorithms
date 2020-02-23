81a03195fa9b98b9689d023e48561185c81d020c
Steven Phan <steven.phan009@gmail.com>
2020-02-22 16:33:01 -0800

    mergesort function works recursively

--- MergeSort/src/app/MergeSort.java
+++ MergeSort/src/app/MergeSort.java
@@ -1,21 +1,54 @@
 package app;
 
+import java.lang.invoke.LambdaMetafactory;
+
 public class MergeSort{
 
-    public static void mergeSort(Integer[] A, left, right){
-
-        if (left<right){
-            middle = (left+right)/2;
+    public static int mergeSort(int[]A, int left, int right){
+        
+        int arrLen = A.length;
+        
+        // check if the length of the array is one and return it since its the 
+        // last one 
+        if (arrLen<=1){
+            return A[0];
+        }
+        else if (left<right){
+            int middle = (left+right)/2;
+            System.out.println("The middle element is: "+A[middle]);
             mergeSort(A,left,middle);
             mergeSort(A,middle+1,right);
             merge(A,left,middle,right);
         }
+        return 0;
     }
 
-    public static void merge(Integer[] A, left, middle, right){
-        
+    public static int[] merge(int[] A, int left, int middle, int right){
+        System.out.println("Testing merge");
+        System.out.println("LEFT: "+A[left]);
+        System.out.println("MID: "+A[middle]);
+        System.out.println("RIGHT: "+A[right]);
+
+        // does merge want to return an array??? 
+        // create left and right arrays
+        int [] retArr;
+        int [] leftArr; 
+        int [] rightArr; 
+        leftArr[leftArr.length]= left; 
+        rightArr[rightArr.length] = right; 
+
+
+        return retArr;
+
     }
-    public static void main(Integer[] args){
+
+    public static void main(String[]args){
+
+        int []testArr = {4,5,1,7,3,3,8};
+        int start = 0; int end = 6;
+        
+        mergeSort(testArr, start, end);
+
         System.out.println("Hello World");
     }
 }
