2d465d9c44cd70fbad1dbc8df1215b69a5f2693f
Steven Phan <steven.phan009@gmail.com>
2020-03-28 20:53:07 -0700

    finished build max heap

--- BuildHeapMax/src/app/BuildMaxHeap.java
+++ BuildHeapMax/src/app/BuildMaxHeap.java
@@ -1,4 +1,6 @@
 package app; 
+import java.lang.Math;
+import java.util.Arrays;
 
 public class BuildMaxHeap{
 
@@ -8,14 +10,14 @@ public class BuildMaxHeap{
         int r = 2*i+2;
         int largest = 0;
 
-        if ((l<=A.length) && (A[l] > A[i])){
+        if ((l<=A.length-1) && (A[l] > A[i])){
             largest = l;    
         }
         else{
             largest = i; 
         }
 
-        if ((r<=A.length) && (A[r] > A[largest])){
+        if ((r<=A.length-1) && (A[r] > A[largest])){
             largest = r;
         }
 
@@ -29,12 +31,39 @@ public class BuildMaxHeap{
         System.out.println("end of max heapify");
     }
 
+    public static void build_max_heap(int[] A){
+        // int heapSize = A.length;
+
+        // TODO: find a better way to fix this problem, but there could a chance
+        //       where there is no data on the right child. To compensate
+        //       insert a null at the end of the array
+        A = Arrays.copyOf(A, A.length+1);
+        
+        int mid = (int) Math.floor(A.length/2 - 1);
+
+        for (int i = mid; i>=0; i--){
+            max_heapify(A, i);
+        }
+
+        // decrease array back to normal
+        A = Arrays.copyOf(A, A.length-1);
+
+       print_array(A); 
+    }
+
+    public static void print_array(int[]A){
+        for (int element:A){
+            System.out.print(element + "  ");
+        }
+    }
+
     public static void main(String[] args){
         // int[] testArr = {4,5,1,7,3,3,8};
-        int[] testArr = {16,4,10,14,7,9,3,2,8,1};
-        max_heapify(testArr, 1
-        );
-        System.out.println("Hello World");
+        // int[] testArr = {16,4,10,14,1,9,3,2,8,7};
+        // int[] testArr = {4,1,3,2,16,9,10,14,8,7};
+        int [] testArr = {4,5,1,7,3,3,8};
 
+        // max_heapify(testArr, 4);
+        build_max_heap(testArr);
     }
 }
