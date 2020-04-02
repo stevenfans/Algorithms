079f73f57062d99b3fca577e0a076e6ba2d36d32
Steven Phan <steven.phan009@gmail.com>
2020-04-01 20:56:21 -0700

    initialized class methods

--- MaxPriorityQueue/MaxPriorityQueue/src/MaxPriorityQueue.java
+++ MaxPriorityQueue/MaxPriorityQueue/src/MaxPriorityQueue.java
@@ -1,7 +1,26 @@
+import java.util.Scanner;
 
 public class MaxPriorityQueue {
 
+    public static void Insert(int[]S,int x){
+        ;
+    }
+
+    public static void Maximum(int[]S){
+        ;
+    }
+
+    public static void ExtractMax(int[]S){
+        ;
+    }
+
+    public static void IncreaseKey(int[]S,int x,int k){
+        ;
+    }
+    
     public static void main(String [] args){
+
+        Scanner user_input = new Scanner(System.in); // Create a Scanner Obj for user input 
         System.out.println("Hello World");
     }
 }
