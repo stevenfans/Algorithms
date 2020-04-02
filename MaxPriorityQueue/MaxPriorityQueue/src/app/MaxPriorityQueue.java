425366e2e178cbbeb493d337fd87687250ae5fcd
Steven Phan <steven.phan009@gmail.com>
2020-04-02 11:22:18 -0700

    refactorred again to use objects

--- MaxPriorityQueue/MaxPriorityQueue/src/app/MaxPriorityQueue.java
+++ MaxPriorityQueue/MaxPriorityQueue/src/app/MaxPriorityQueue.java
@@ -1,34 +1,32 @@
-import java.lang.reflect.Array;
+package app; 
+// import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class MaxPriorityQueue {
 
-    // class Heap{
-    //     int[] S;
-    // }
-    int [] S;
-    int x; //insert 
+    int[] node;
+    int x; // insert SSS
 
     // ============= BUILD MAX HEAP METHODS==========================
-    public static void max_heapify(int[] A, int i){
+    public static void max_heapify(int[] A, int i) {
+
         // based from 6.2 algorithm
-        int l = 2*i+1;
-        int r = 2*i+2;
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
         int largest = 0;
 
-        if ((l<=A.length-1) && (A[l] > A[i])){
-            largest = l;    
-        }
-        else{
-            largest = i; 
+        if ((l <= A.length - 1) && (A[l] > A[i])) {
+            largest = l;
+        } else {
+            largest = i;
         }
 
-        if ((r<=A.length-1) && (A[r] > A[largest])){
+        if ((r <= A.length - 1) && (A[r] > A[largest])) {
             largest = r;
         }
 
-        if (largest != i){
+        if (largest != i) {
             // swap values
             int tmp = A[i];
             A[i] = A[largest];
@@ -37,63 +35,60 @@ public class MaxPriorityQueue {
         }
     }
 
-    public static int[] build_max_heap(int[] A){
+    public static int[] build_max_heap(int[] A) {
         // int heapSize = A.length;
-        
-        A = Arrays.copyOf(A, A.length+1);
-        
-        int mid = (int) Math.floor(A.length/2 - 1);
 
-        for (int i = mid; i>=0; i--){
+        A = Arrays.copyOf(A, A.length + 1);
+
+        int mid = (int) Math.floor(A.length / 2 - 1);
+
+        for (int i = mid; i >= 0; i--) {
             max_heapify(A, i);
         }
 
         // decrease array back to normal
-        A = Arrays.copyOf(A, A.length-1);
+        A = Arrays.copyOf(A, A.length - 1);
 
         // determine the height: 2^(h+1) - 1
-        // int height = (int) (Math.log(heapSize+1)/Math.log(2))-1; 
+        // int height = (int) (Math.log(heapSize+1)/Math.log(2))-1;
 
         System.out.print("Outputted Max Heap: ");
-        print_array(A); 
-        return A; 
+        print_array(A);
+        return A;
         // System.out.println("\nHeight: " + height);
     }
 
-    public static void print_array(int[]A){
-        for (int element:A){
+    public static void print_array(int[] A) {
+        for (int element : A) {
             System.out.print(element + "  ");
         }
     }
     // =================================================================
 
-
-    public static int[] Insert(int[]S,int x){
-        
-        int [] new_S; 
-        new_S = Arrays.copyOf(S, S.length+1); 
-        new_S[S.length] = x; 
-
-        return new_S;
+    public void Insert(int[]S,int x){
+        node = Arrays.copyOf(S,S.length+1);
+        node[node.length-1] = x;  
+        // System.out.print("STOP"); 
     }
 
-    public static int Maximum(int[]S){
+    public int Maximum(int[]S){
         return S[0];
     }
 
-    // public static int ExtractMax(int[]S){
-    //     if (S.length<1){
-    //         System.out.println("Heap Underflow");
-    //     }
-    //     else{
-    //         int max = S[0]; 
-    //         S[0] = S[S.length]; 
-    //         int [] A = Arrays.copyOf(S,S.length-1); 
-    //         max_heapify(A, 1);
-    //         max = 
-    //     }
-    //     return max; 
-    // }
+    public int ExtractMax(int[]S){
+        int max; 
+        if (S.length<1){
+            System.out.println("Heap Underflow");
+            max = 0; 
+        }
+        else{
+            max = S[0]; 
+            S[0] = S[S.length-1]; 
+            node = Arrays.copyOf(S,S.length-2); 
+            max_heapify(node, 1);
+        }
+        return max; 
+    }
 
     public static void IncreaseKey(int[]S,int x,int k){
         ;
@@ -104,7 +99,7 @@ public class MaxPriorityQueue {
 
         // MaxPriorityQueue heap = new MaxPriorityQueue(); 
         Scanner input = new Scanner(System.in);
-        int [] new_S = {0}; 
+        // int [] new_S = {0}; 
 
         switch(option){
             case 1: 
@@ -113,17 +108,19 @@ public class MaxPriorityQueue {
                 // create scanner object for the integer to input
                 System.out.print("Input the integer you want inserted: ");
                 int x = input.nextInt();
-                heap.S = Insert(heap.S, x);
-                // heap.Insert(S, x);
+                heap.Insert(heap.node, x);
+
                 break;
             case 2:
                 System.out.println("\nMaximum\n");
-                int max = Maximum(heap.S);
+                // int max = Maximum(heap.S);
+                int max = heap.Maximum(heap.node);
                 System.out.print("Maximum value is: ");
                 System.out.println(max);
                 break;
             case 3:
                 System.out.println("\nExtract-Max\n");
+                heap.ExtractMax(heap.node);
                 break;
             case 4: 
                 System.out.println("\nIncrease-key\n");
@@ -140,11 +137,14 @@ public class MaxPriorityQueue {
         } 
 
         // input.close(); //close scanner
-        // return new_S;
+
         return heap;
     } 
     
     public static void main(String [] args){
+
+
+        // Create HEAP Object
 
         int [] testArr = {5,10,8,13,2,17,12};
 
@@ -153,14 +153,14 @@ public class MaxPriorityQueue {
         // Heap heap = new MaxPriorityQueue(). new Heap();
         // heap.S = testArr;
         MaxPriorityQueue heap = new MaxPriorityQueue();
-        heap.S = testArr;
+        heap.node = testArr;
 
         Scanner user_input = new Scanner(System.in); // Create a Scanner Obj for user input 
         
         while(option!=5){
 
             
-            heap.S = build_max_heap(heap.S);
+            heap.node = build_max_heap(heap.node);
 
             System.out.println("\nChoose from the following options:\n");
             System.out.println("1. Insert");
